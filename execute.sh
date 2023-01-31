115ebd643ac2a0769f0b160676bc5dcfc1be6703
Eric <elipe@goraft.tech>
2023-01-31 14:59:27 -0700

    - removed directory manipulation

--- execute.sh
+++ execute.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-pushd $HOME
 
 if [ $# -eq 0 ]
 then
   echo "No arguments supplied, expected Python script to execute."
-  popd
   exit 1
 fi
 
 $SPARK_HOME/bin/spark-submit --packages graphframes:graphframes:0.8.2-spark3.2-s_2.12 $1
-
-popd
