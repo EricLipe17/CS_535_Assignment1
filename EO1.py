5eaeb5f3aa006658b5051d9ea2b5413b7b0c8f9e
Eric <elipe@goraft.tech>
2023-01-31 14:29:33 -0700

    - fixing vertex and edge lists

--- EO1.py
+++ EO1.py
@@ -7,40 +7,37 @@ cwd = os.getcwd()
 print(cwd)
 
 edges_file = os.path.join(cwd, "citations.txt")
-properties_file = os.path.join(cwd, "published-dates.txt")
+vertex_file = os.path.join(cwd, "published-dates.txt")
 years = [str(year) for year in range(1993, 2003)]
 
-all_vertices = dict()
+# Get all edges
 edges = list()
 with open(edges_file) as f:
     for line in f:
         if line[0] != "#":
-            vertices = line.split()
-            for vertex in vertices:
-                all_vertices[vertex] = None
-            vertices.append("cites")
-            edges.append(tuple(vertices))
+            edges.append(tuple(line.split()))
 
-# Associate vertices with their properties
-with open(properties_file) as f:
+# Get vertices
+vertices = list()
+with open(vertex_file) as f:
     for line in f:
         if line[0] != "#":
             vertex_prop = line.split()
-            # TODO: This does not handle cross-referenced papers yet
+            # TODO: This does not handle cross-referenced papers yet. Because of this the number of nodes in the
+            #  graph is incorrect!
             if vertex_prop[0][:2] == "11":
                 continue
 
             # Only interested in the year the paper was published
             vertex_prop[1] = vertex_prop[1][:4]
-            if vertex_prop[0] in all_vertices:
-                all_vertices[vertex_prop[0]] = tuple(vertex_prop)
+            vertices.append(tuple(vertex_prop))
 
 # Create the spark context
 spark = SparkSession.builder.appName('Assignment_1').getOrCreate()
 sqlContext = SQLContext(spark)
 
 # Create a Vertex DataFrame with unique ID column "id"
-vertex_df = sqlContext.createDataFrame(list(all_vertices.values()), ["id", "published_year"])
+vertex_df = sqlContext.createDataFrame(vertices, ["id", "published_year"])
 # Create an Edge DataFrame with "src" and "dst" columns
 edge_df = sqlContext.createDataFrame(edges, ["src", "dst", "relationship"])
 
