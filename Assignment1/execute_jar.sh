63cee7e795e5396124f19c78104910cf9c627a1a
Eric <elipe@goraft.tech>
2023-02-16 14:46:36 -0700

    - adding script to execute jar more quickly

---
+++ Assignment1/execute_jar.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+pushd ~
+if [[ "$1" != "" ]]; then
+    JAR="$1"
+else
+    JAR=.
+fi
+
+if [[ "$2" != "" ]]; then
+    CLS="$2"
+else
+    CLS=org.ass1.Main
+fi
+
+if [[ "$3" != "" ]]; then
+    MODE="$3"
+else
+    MODE=client
+fi
+
+$SPARK_HOME/bin/spark-submit --deploy-mode $MODE --supervise --class $CLS $JAR
+popd
