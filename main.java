8ada02c599616ccffae99ecb3c16dbcc066789f8
saikiranpennam <49566837+saikiranpennam@users.noreply.github.com>
2023-02-15 20:07:46 -0700

    Update main.java

--- main.java
+++ main.java
@@ -1,56 +1,120 @@
-import java.util.ArrayList;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.expressions.Window;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+import static org.apache.spark.sql.functions.*;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import scala.Tuple2;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Serializable;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
-public class EffectiveDiameter {
-    public static void main(String[] args) {
-        // Initialize Spark configuration and context
-        SparkConf conf = new SparkConf().setAppName("EffectiveDiameter");
-        JavaSparkContext sc = new JavaSparkContext(conf);
+public class Main() {
+        public static void main(String[] args)  {
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Assignment1")
+                .getOrCreate();
+        spark.sparkContext().setLogLevel("ERROR");
+        SQLContext sqlContext = new SQLContext(spark);
 
-        // Load the citation edges and published dates into RDDs
-        JavaRDD<String> citationEdges = sc.textFile("citations.txt");
-        JavaRDD<String> publishedDates = sc.textFile("published-dates.txt");
-
-        // Create a pair RDD of the citation edges with the publication year as the key
-        JavaPairRDD<Integer, Tuple2<Integer, Integer>> edgesByYear = citationEdges.flatMapToPair(line -> {
-            String[] fields = line.split(" ");
-            int fromNode = Integer.parseInt(fields[0]);
-            int toNode = Integer.parseInt(fields[1]);
-            List<Tuple2<Integer, Tuple2<Integer, Integer>>> results = new ArrayList<>();
-            for (int year = 1993; year <= 2003; year++) {
-                results.add(new Tuple2<>(year, new Tuple2<>(fromNode, toNode)));
-            }
-            return results.iterator();
+                // Create spark structs to represent columns for dataframes
+        StructType vertex_df_cols = DataTypes.createStructType(new StructField[] {
+                DataTypes.createStructField("id", DataTypes.StringType, true),
+                DataTypes.createStructField("published_date", DataTypes.StringType, true)
         });
 
-        // Create a pair RDD of the publication year and publication date for each vertex
-        JavaPairRDD<Integer, Integer> publicationDates = publishedDates.flatMapToPair(line -> {
-            String[] fields = line.split(" ");
-            int node = Integer.parseInt(fields[0]);
-            int publicationYear = Integer.parseInt(fields[1]);
-            List<Tuple2<Integer, Integer>> results = new ArrayList<>();
-            for (int year = 1993; year <= 2003; year++) {
-                results.add(new Tuple2<>(year, node));
-            }
-            return results.iterator();
+        StructType edge_df_cols = DataTypes.createStructType(new StructField[] {
+                DataTypes.createStructField("src", DataTypes.StringType, true),
+                DataTypes.createStructField("dst", DataTypes.StringType, true)
         });
 
-        // Calculate the effective diameter for each year
-        for (int year = 1993; year <= 2003; year++) {
-            // Filter the edges and publication dates for the current year
-            JavaPairRDD<Integer, Tuple2<Integer, Integer>> yearEdges = edgesByYear.filter(edge -> edge._1 == year);
-            JavaPairRDD<Integer, Integer> yearPublicationDates = publicationDates.filter(node -> node._1 == year);
+        Dataset<Row> edge_df = sqlContext.read().option("header","true").option("sep", "\t").schema(edge_df_cols)
+                                            .csv("hdfs:///citations.txt");
+        edge_df = edge_df.filter(not(edge_df.col("src").contains("#")));
+        Dataset<Row> edge_src = edge_df.select("src").distinct().withColumnRenamed("src", "id");
+        Dataset<Row> edge_dst = edge_df.select("dst").distinct().withColumnRenamed("dst", "id");
+        Dataset<Row> vertex_df = edge_src.union(edge_dst).distinct().withColumnRenamed("id", "id_old");
 
-            // Calculate the shortest paths between all node pairs
+        Dataset<Row> properties = sqlContext.read().option("header","true").option("sep", "\t").schema(vertex_df_cols)
+                .csv("hdfs:///published-dates.txt");
+        properties = properties.withColumn("published_year", properties.col("published_date").substr(0,4)).drop("published_date");
+
+        vertex_df = properties.join(vertex_df, properties.col("id").equalTo(vertex_df.col("id_old"))).drop("id_old");
+
+        // Create data containers
+        ArrayList<ArrayList<Long>> vertices_edges_by_year = new ArrayList<>();
+        ArrayList<ArrayList<Long>> paths_by_year = new ArrayList<>();
+        long num_vertices;
+        long num_edges;
             
-            // I am stuck here, have to implement BFS 
-            
-            // Calculate the effective diameter for the current year
+        // Execute algorithms on each subgraph
+        for (long year = 1993; year < 2003; year++) {
+            // Collect number of vertices for this subgraph
+            Dataset<Row> vertices_by_year_df = vertex_df.filter(vertex_df.col("published_year").$less$eq(year));
+            num_vertices = vertices_by_year_df.count();
+
+            // Collect number of out edges for this subgraph
+            Dataset<Row> edges_by_year = vertices_by_year_df.join(edge_df, vertices_by_year_df.col("id").equalTo(edge_df.col("src")), "inner");
+            num_edges = (long) edges_by_year.groupBy("src").count().select(sum("count")).collectAsList().get(0).get(0);
+
+            // Evaluate g(d) where 1 <= d <= 4
+            ArrayList<Long> distribution = new ArrayList<>(4);
+            distribution.add(vertices_by_year_df.count());
+            Dataset<Row> dst = edges_by_year.select("dst").withColumnRenamed("dst", "dst_original").dropDuplicates();
+            for (int i = 0; i < 3; i++) {
+                Dataset<Row> new_src = dst.alias("dst").join(edges_by_year.alias("edges"), col("dst.dst_original").equalTo(col("edges.src")), "inner").drop("dst_original");
+                dst = new_src.select("dst").withColumnRenamed("dst", "dst_original").dropDuplicates();
+                distribution.add(dst.distinct().count());
+            }
+
+         // Save vertex and edge counts for this subgraph
+         ArrayList<Long> vertices_edges = new ArrayList<>(2);
+         vertices_edges.add(num_vertices);
+         vertices_edges.add(num_edges);
+         vertices_edges_by_year.add(vertices_edges);
+
+         // Save distribution for this subgraph
+         paths_by_year.add(distribution);
         }
-    }
-}
+
+        // Determine total number of connected node pairs
+        ArrayList<Long> connected_node_pairs = new ArrayList<>(10);
+        for (int i = 0; i < 10; i++) {
+            long count = 0;
+            for (ArrayList<Long> paths: paths_by_year) {
+                count += paths.get(i);
+            }
+            connected_node_pairs.add(count);
+        }
+        long total_connected_node_pairs = connected_node_pairs.stream().mapToLong(Long::longValue).sum();
+
+        // Calculate effective diameter for each year
+        for (int i = 0; i < 9; i++) {
+            long cumulative_count = 0;
+            int lower_bound = i + 1;
+            int upper_bound = i + 2;
+            long d_lower = lower_bound;
+            long d_upper = upper_bound;
+            long g_lower = connected_node_pairs.get(i);
+            long g_upper = connected_node_pairs.get(i + 1);
+            for (ArrayList<Long> paths: paths_by_year) {
+                cumulative_count += paths.get(i);
+            }
+            while (d_upper <= 4 && cumulative_count / (double)total_connected_node_pairs < 0.9) {
+                cumulative_count += connected_node_pairs.get(upper_bound - 1);
+                upper_bound++;
+                d_upper++;
+                g_upper = connected_node_pairs.get(upper_bound - 1);
+            }
+            double x = (cumulative_count / (double)total_connected_node_pairs - (g_upper / (double)total_connected_node_pairs)) /
+                    ((g_lower / (double)total_connected_node_pairs) - (g_upper / (double)total_connected_node_pairs));
+            double effective_diameter = d_lower + x;
+            System.out.println("Effective diameter for year " + (i + 1993) + ": " + effective_diameter);
+        }//end for loop
+    }//end function main
+}// end class Main
