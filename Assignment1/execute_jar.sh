e70e1bf5262052789e893763adbceb3ea51aadd6
Eric <elipe@goraft.tech>
2023-02-16 14:58:19 -0700

    - Updating script

--- Assignment1/execute_jar.sh
+++ Assignment1/execute_jar.sh
@@ -9,13 +9,13 @@ fi
 if [[ "$2" != "" ]]; then
     CLS="$2"
 else
-    CLS=org.ass1.Main
+    CLS="org.ass1.Main"
 fi
 
 if [[ "$3" != "" ]]; then
     MODE="$3"
 else
-    MODE=client
+    MODE="client"
 fi
 
 $SPARK_HOME/bin/spark-submit --deploy-mode $MODE --supervise --class $CLS $JAR
